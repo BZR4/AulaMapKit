4296f0b8d9cbf4bb8076f11dd86cf20e895b3917
Esdras B Silva <bzr4@icloud.com>
2021-12-04 07:34:37 -0500

    - Commit inicial?   - Ajustes ininiais para configura;'ao e uso de mapas.

--- AulaMapKit/ViewController.swift
+++ AulaMapKit/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    
+    @IBOutlet weak var outletMapKit: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    var userLocation = CLLocation()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        outletMapKit.showsUserLocation = true
     }
-
-
+    
+     //Parametros deconfiguracao
+    func setupLocationManager(){
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+     //Localizacao do Usuario
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if locations.count > 0 {
+            if let location = locations.last{
+                userLocation = location
+                print("A localizacao di susuario eh LAT: \(userLocation.coordinate.latitude) - LON:\(userLocation.coordinate.longitude)")
+            }
+        }
+    }
 }
 
